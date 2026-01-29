a4430b5478a407db820c34085e96d07974392105
Faith Nkirote <nkirotef054@gmail.com>
2026-01-29 08:59:47 +0300

    Style: Signup form styling

--- FRONTEND/src/Signup.jsx
+++ FRONTEND/src/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import "./Auth.css";
+import "./signup.css";
 
 const Signup = () => {
   const navigate = useNavigate();

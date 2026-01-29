7a394cd5c9d115d984bc3b9493f98e516ed020e8
Faith Nkirote <nkirotef054@gmail.com>
2026-01-29 22:30:23 +0300

    Update landing content

--- FRONTEND/src/Landing.jsx
+++ FRONTEND/src/Landing.jsx
@@ -5,7 +5,7 @@ import "./Landing.css";
 const Landing = () => {
   return (
     <div className="landing-container">
-        
+
       {/* HERO */}
       <section className="hero-section">
         <div className="hero-overlay" />

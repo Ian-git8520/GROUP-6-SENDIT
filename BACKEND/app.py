9b0ec0af274d2ccf5a21956da432b1aa770a0427
Mercy <mercy.mwangi2@student.moringaschool.com>
2026-02-12 07:54:31 +0300

    changes

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -5,18 +5,17 @@ from flask_cors import CORS
 
 from resources.user import UserListResource, UserResource
 from resources.delivery import DeliveryListResource
-from resources.user_delivery import UserDeliveryResource
 from resources.profile import Profile
-from resources.auth import Register, Login, Logout
+from resources.auth import Register, Login
 from resources.admin_delivery import AdminDeliveryResource
-from resources.rider import RiderListResource, DriverProfileResource, DriverDeliveryResource, DriverDeliveryListResource
-from resources.track_delivery import TrackDeliveryResource
+from resources.rider import RiderListResource
+
+
+
 
 
 app = Flask(__name__)
-CORS( app,supports_credentials=True)
-
-
+CORS(app)
 api = Api(app)
 
 
@@ -26,7 +25,6 @@ init_db()
 
 api.add_resource(Register, "/auth/register", strict_slashes=False)
 api.add_resource(Login, "/auth/login", strict_slashes=False)
-api.add_resource(Logout, "/auth/logout", strict_slashes=False)
 
 
 api.add_resource(UserListResource, "/users", strict_slashes=False)
@@ -34,8 +32,8 @@ api.add_resource(UserResource, "/users/<int:user_id>", strict_slashes=False)
 
 
 api.add_resource(DeliveryListResource, "/deliveries", strict_slashes=False)
-api.add_resource(UserDeliveryResource, "/user/deliveries/<int:delivery_id>", strict_slashes=False)
 
+# for the current logged-in user
 api.add_resource(Profile, "/profile", strict_slashes=False)
 
 api.add_resource(
@@ -44,17 +42,8 @@ api.add_resource(
 )
 
 api.add_resource(RiderListResource, "/riders", strict_slashes=False)
-api.add_resource(DriverProfileResource, "/driver/profile", strict_slashes=False)
-api.add_resource(DriverDeliveryListResource, "/driver/deliveries", strict_slashes=False)
-api.add_resource(DriverDeliveryResource, "/driver/deliveries/<int:delivery_id>", strict_slashes=False)
-
-api.add_resource(
-    TrackDeliveryResource,
-    "/deliveries/<int:delivery_id>/track"
-)
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port= 5001)
-   
+    app.run(debug=True)
